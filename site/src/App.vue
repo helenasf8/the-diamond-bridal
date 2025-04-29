8bebea9fa226ea0560aa5113c697e1160867412f
helenasf8 <helenadasilvafortunato.if@gmail.com>
2025-04-29 11:57:54 -0300

    FEAT-1 helena

--- site/src/App.vue
+++ site/src/App.vue
@@ -3,7 +3,32 @@
 </script>
 
 <template>
-
+  <header>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Termos</a>
+            </li>
+            <li>
+                <a href="#">Equipe</a>
+            </li>
+            <li>
+                <a href="#">Envio</a>
+            </li>
+            <li>
+                <a href="#">Devoluções</a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <span class="mdi mdi-cart" />
+                <span class="mdi mdi-heart" />
+                <span class="mdi mdi-account" />
+                <span class="mdi mdi-truck" />
+            </li>
+        </ul>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
